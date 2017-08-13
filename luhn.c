f730bbeaf293b50844e232d36c61ca61f6db6bc2
Jan Cajthaml <jan.cajthaml@gmail.com>
2017-08-13 20:21:46 +0200

    branch free implementation

--- luhn.c
+++ luhn.c
@@ -1,10 +1,14 @@
-const int m[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8, 1, 3, 5, 7, 9 };
+const int m[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 6,
+  8, 1, 3, 5, 7, 9
+};
 
 int digit(const char* data) {
   unsigned int d = 0;
   unsigned long length = sizeof(*data);
   const char* current = (const char*) data;
-	goto *((*current == '\0') ? &&result :  &&loop);
+  goto *((*current == '\0') ? &&result : &&loop);
 
 loop:
   d += ((length & 1) ? m[*current++] : (*current++ - 48));
